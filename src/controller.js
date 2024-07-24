183593b05d3b8696b8ce910410936071ee289300
dpruel <dvpruel@gmail.com>
2024-07-24 18:54:48 +0300

    HOT FIX

--- src/controller.js
+++ src/controller.js
@@ -1,14 +1,14 @@
 export default class Controller {
     constructor(game, view) {
         this.game = game;
-        this.view = view; 
+        this.view = view;
         this.intervalId = null;
         this.isPlaying = false;
-        
+
         document.addEventListener('keydown', this.handleKeyDown.bind(this));
         document.addEventListener('keyup', this.handleKeyUp.bind(this));
 
-        this.view.renderStartScreen(); 
+        this.view.renderStartScreen();
     }
 
     update() {
@@ -42,14 +42,13 @@ export default class Controller {
         } else {
             this.view.renderMainScreen(state);
         }
-        
     }
 
     startTimer() {
-        const speed = 1000 - this.game. getState().level * 100;
+        const speed = 1000 - this.game.getState().level * 100;
         if (!this.intervalId) {
             this.intervalId = setInterval(() => {
-                this.update()
+                this.update();
             }, speed > 0 ? speed : 100);
         }
     }
@@ -59,7 +58,6 @@ export default class Controller {
             clearInterval(this.intervalId);
             this.intervalId = null;
         }
-       
     }
 
     handleKeyDown(event) {
@@ -75,31 +73,40 @@ export default class Controller {
                     this.play();
                 }
                 break;
-                this.view.renderMainScreen(this.game.getState)
             case 37: // LEFT ARROW
-                this.game.movePieceLeft();
-                this.updateView;
+                if (this.isPlaying) {
+                    this.game.movePieceLeft();
+                    this.updateView();
+                }
                 break;
             case 38: // UP ARROW
-                 this.game.rotatePiece();
-                 this.updateView();
+                if (this.isPlaying) {
+                    this.game.rotatePiece();
+                    this.updateView();
+                }
                 break;
             case 39: // RIGHT ARROW
-                this.game.movePieceRight();
-                this.updateView();
+                if (this.isPlaying) {
+                    this.game.movePieceRight();
+                    this.updateView();
+                }
                 break;
             case 40: // DOWN ARROW
-                this.stopTimer();
-                this.game.movePieceDown();
-                this.updateView();
+                if (this.isPlaying) {
+                    this.stopTimer();
+                    this.game.movePieceDown();
+                    this.updateView();
+                }
                 break;
         }
     }
 
-    handleKeyUp() {
+    handleKeyUp(event) {
         switch (event.keyCode) {
             case 40: // DOWN ARROW
-                this.startTimer();
+                if (this.isPlaying) {
+                    this.startTimer();
+                }
                 break;
         }
     }
